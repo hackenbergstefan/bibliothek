24043590820aa63175aa9172f20ddaeb0cad6b54
zero-git <hackenberg.stefan@t-online.de>
2013-10-07 17:21:59 +0200

    -

---
+++ src/util/MutableInteger.java
@@ -0,0 +1,13 @@
+package util;
+public class MutableInteger {
+    private int value;
+    public MutableInteger(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
